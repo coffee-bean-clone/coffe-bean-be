eafc640b600b4e7160c94ec8fa012a54ef8d240e
강경규 <qwzx16@naver.com>
2024-03-14 23:51:08 +0900

    FEAT: 상품 타입 생성

---
+++ types/TProduct.ts
@@ -0,0 +1,13 @@
+type TProduct = {
+  title: string;
+  price: number;
+  // createdAt: Date;
+  mainCategory: string;
+  subCategory: string;
+  isSale: boolean;
+  isNew: boolean;
+  detailImage: string;
+  productImages: string[];
+};
+
+export default TProduct;
